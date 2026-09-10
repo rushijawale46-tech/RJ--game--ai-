7cca621173c394ab3b81959937b5689836fd8f60
rushijawale46-tech <rushijawale46@gmail.com>
2026-09-10 11:19:32 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "rj-game-india-v1.0.0";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
