626265db38aa53f715ff7010b1e5a9ac70b96a0d
rushijawale46-tech <rushijawale46@gmail.com>
2026-09-10 14:26:49 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = "rj-game-india-v1.0.1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./virat-kohli.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
